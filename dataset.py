de34ae8b5bd19085eb7efe69abf9a462cc5fb0e4
Remomare <75660801+Remomare@users.noreply.github.com>
2021-11-17 02:25:55 +0900

    dataset setting update

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import os
+from torch._C import dtype
 from tqdm.auto import tqdm
 
 import pandas as pd
@@ -11,7 +12,8 @@ class  CustomDataset(torch.utils.data.Dataset):
     def __init__(self, args, data_path:str):
 
         self.data_path = data_path
-        self.datas = []
+        self.datas_1 = []
+        self.datas_2 = []
 
         df = pd.read_csv(os.path.join(data_path), sep='\t', header=0)
 
@@ -23,30 +25,39 @@ class  CustomDataset(torch.utils.data.Dataset):
 
         if args.data_column_index is not None:
             ingredient_column = df.columns[args.data_column_index]
+        if args.time_column_index is not None:
+            timestamp_column = df.columns[args.time_column_index]
+        
         else:
             raise ValueError('data_column_index must be specified if data_path is a csv file')
         
-        datas = df[ingredient_column]
+        datas_1 = df[ingredient_column]
+        datas_2 = df[ingredient_column+1]
         targets = df[target_colunm]
+        timestamps = df[timestamp_column]
         
         if args.max_seq_len is None:
-            args.max_seq_len = max(len(t.split()) for t in datas)
+            args.max_seq_len = max(len(t.split()) for t in datas_1)
         
-        tensor_ingredient = torch.tensor([])
+        tensor_ingredient_1 = torch.tensor([])
+        tensor_ingredient_2 = torch.tensor([])
         tensor_target = torch.tensor([])
+        tensor_timestamp = torch.tensro([])
 
+        
         for idx in tqdm(df.index, desc=f'Loading {self.data_path}'):
             data = {}
 
-            if args.time_column_index is not None:
-                data['time'] = torch.tensor(df[time_column].astype(int), dtype=torch.float64)
-
             #data['ingredient'] = torch.tensor(df[ingredient_column][idx], dtype=torch.long)
 
-            ingredient = [datas[idx]]
+            ingredient_1 = [datas_1[idx]]
+            ingredient_2 = [datas_2[idx]]
             target = [targets[idx]]
-            data['ingredient'] = ingredient
+            timestamp_ = [timestamps[idx]]
+            data['ingredient_1'] = ingredient_1
+            data['ingredient_2'] = ingredient_2
             data['target'] = target
+            data['time'] = timestamp_
 
             """
             if args.min_seq_len is not None and len(ingredient) < args.min_seq_len:
@@ -56,21 +67,33 @@ class  CustomDataset(torch.utils.data.Dataset):
             else:
                 tensor_ingredient = torch.tensor(ingredient, dtype=torch.long)
             """
-            scala_ingredient = torch.tensor(ingredient, dtype=torch.long)
+            scala_ingredient_1 = torch.tensor(ingredient_1, dtype=torch.long)
+            scala_ingredient_2 = torch.tensor(ingredient_2, dtype=torch.long)
             scala_target = torch.tensor(target, dtype=torch.long)
+            scala_timestamp = torch.tensor(timestamp_, dtype=torch.long)
 
-            tensor_ingredient = torch.cat((tensor_ingredient,scala_ingredient))
+            tensor_ingredient_1 = torch.cat((tensor_ingredient_1,scala_ingredient_1))
+            tensor_ingredient_2 = torch.cat((tensor_ingredient_2,scala_ingredient_2))
+            tensor_timestamp = torch.cat((tensor_timestamp, scala_timestamp))
+
             tensor_target = torch.cat((tensor_target, scala_target))
 
-            if len(tensor_ingredient) == (args.seq_len):
+
+            if len(tensor_ingredient_1) == (args.seq_len):
 
                 if args.vae_setting==True:
-                    src_input = tensor_ingredient
-                    tgt_input = torch.cat([torch.tensor(1, dtype=torch.long).unsqueeze(0), tensor_ingredient]).long()
-                    tgt_output = torch.cat([tensor_ingredient, torch.tensor(2, dtype=torch.long).unsqueeze(0)]).long()
+                    src_input_1 = tensor_ingredient_1
+                    src_input_2 = tensor_ingredient_2
+                    timestamp = tensor_timestamp
+                    tgt_input_1 = torch.cat([torch.tensor(1, dtype=torch.long).unsqueeze(0), tensor_ingredient_1]).long()
+                    tgt_output_1 = torch.cat([tensor_ingredient_1, torch.tensor(2, dtype=torch.long).unsqueeze(0)]).long()
+                    tgt_input_2 = torch.cat([torch.tensor(1, dtype=torch.long).unsqueeze(0), tensor_ingredient_2]).long()
+                    tgt_output_2 = torch.cat([tensor_ingredient_2, torch.tensor(2, dtype=torch.long).unsqueeze(0)]).long()
 
-                    for d, d_name in [(src_input, 'src_input'), (tgt_input, 'tgt_input'), (tgt_output, 'tgt_output')]:
-                        pad_len = args.max_seq_len - len(d)
+                    for d, d_name in [(src_input_1, 'src_input_1'), (tgt_input_1, 'tgt_input_1'), (tgt_output_1, 'tgt_output_1'), 
+                                      (src_input_2, 'src_input_2'), (tgt_input_2, 'tgt_input_2'), (tgt_output_2, 'tgt_output_2'),
+                                      (timestamp, 'timestamp')]:
+                        pad_len = (args.max_seq_len+1) - len(d)
                         if pad_len > 0:
                             padding  = torch.tensor(0, dtype=torch.long).unsqueeze(0).repeat(pad_len)
                             data[d_name] = torch.cat([d, padding]).long()
@@ -79,11 +102,13 @@ class  CustomDataset(torch.utils.data.Dataset):
                         else:
                             raise ValueError('Sequence length is greater than max_seq_len')
             
-                    data['length'] = tgt_output.size(0)
+                    data['length'] = tgt_output_1.size(0)
                     self.datas.append(data)
-                    tensor_ingredient = torch.tensor([])
+                    tensor_ingredient_1 = torch.tensor([])
+                    tensor_ingredient_2 = torch.tensor([])
                     tensor_target = torch.tensor([])
-
+                    tensor_timestamp = torch.tensor([])
+                """
                 else:
                     src_input = tensor_ingredient
                     tgt_input = torch.cat([torch.tensor(1, dtype=torch.long).unsqueeze(0), tensor_ingredient]).long()
@@ -104,6 +129,7 @@ class  CustomDataset(torch.utils.data.Dataset):
                     self.datas.append(data)
                     tensor_ingredient = torch.tensor([])
                     tensor_target = torch.tensor([])
+                """
 
     def __len__(self):
         return len(self.datas)
--- epoch.py
+++ epoch.py
@@ -19,17 +19,20 @@ def train_epoch(args, epoch_idx, model, dataloader, optimizer, scheduler, loss_f
     epoch_acc = 0
     if args.vae_setting == True:
         for batch_idx, batch in enumerate(tqdm(dataloader, desc=f'TRAIN EPOCH {epoch_idx}/{args.epoch}')):
-            src_input = batch['src_input'].to(device)
-            tgt_input = batch['tgt_input'].to(device)
-            tgt_output = batch['tgt_output'].to(device)
-            timestamp = batch['time'].to(device)
+            src_input_1 = batch['src_input_1'].to(device)
+            tgt_input_1 = batch['tgt_input_1'].to(device)
+            tgt_output_1 = batch['tgt_output_1'].to(device)
+            src_input_2 = batch['src_input_2'].to(device)
+            tgt_input_2 = batch['tgt_input_2'].to(device)
+            tgt_output_2 = batch['tgt_output_2'].to(device)
+            timestamp = batch['timestamp'].to(device)
             length = batch['length'].to(device)
 
-            log_prob, mean, log_var, z = model(src_input, tgt_input, length, timestamp)
-            NLL_loss, KL_loss, KL_weight = loss_fn(log_prob, tgt_output, length, mean, log_var, kl_anneal_step)
+            log_prob, mean, log_var, z = model(src_input_1, tgt_input_1, length, timestamp)
+            NLL_loss, KL_loss, KL_weight = loss_fn(log_prob, tgt_output_1, length, mean, log_var, kl_anneal_step)
             loss = (NLL_loss + KL_weight * KL_loss) / args.batch_size
 
-            batch_acc = vae_batch_accuracy(log_prob, tgt_output, length)
+            batch_acc = vae_batch_accuracy(log_prob, tgt_output_1, length)
             epoch_acc += batch_acc.detach()
 
             optimizer.zero_grad()
@@ -37,6 +40,24 @@ def train_epoch(args, epoch_idx, model, dataloader, optimizer, scheduler, loss_f
             optimizer.step()
             scheduler.step()
             kl_anneal_step += 1
+
+            if batch_idx < 1:
+                print("timestamp")
+                print(timestamp)
+                print("src_input")
+                print(src_input_1)
+                print("tgt_input")
+                print(tgt_input_1)
+                print("tgt_output")
+                print(tgt_output_1)
+                print('log_prob')
+                print(log_prob)
+                print('mean')
+                print(mean)
+                print('log_var')
+                print(log_var)
+                print('z')
+                print(z)
 
         # Logging
             if batch_idx % args.log_interval == 0 or batch_idx == len(dataloader) - 1:
@@ -53,15 +74,20 @@ def train_epoch(args, epoch_idx, model, dataloader, optimizer, scheduler, loss_f
                 writer.add_scalar('TRAIN/Batch_Accuracy', batch_acc.item(), batch_idx+(epoch_idx*len(dataloader)))
     else:
          for batch_idx, batch in enumerate(tqdm(dataloader, desc=f'TRAIN EPOCH {epoch_idx}/{args.epoch}')):
-            src_input = batch['src_input'].to(device)
-            tgt_input = batch['tgt_input'].to(device)
-            tgt_output = batch['tgt_output'].to(device)
+            src_input_1 = batch['src_input_1'].to(device)
+            tgt_input_1 = batch['tgt_input_1'].to(device)
+            tgt_output_1 = batch['tgt_output_1'].to(device)
+            src_input_2 = batch['src_input_2'].to(device)
+            tgt_input_2 = batch['tgt_input_2'].to(device)
+            tgt_output_2 = batch['tgt_output_2'].to(device)
+            timestamp = batch['timestamp'].to(device)
+            length = batch['length'].to(device)
             time = batch['time'].to(device)
             length = batch['length'].to(device)
             """
             need check
             """
-            z, output, tgt_embedding = model(src_input, tgt_input, length, time)
+            z, output, tgt_embedding = model(src_input_1, tgt_input_1, length, time)
 
             loss = loss_fn(output, tgt_embedding)
 
@@ -91,7 +117,7 @@ def train_epoch(args, epoch_idx, model, dataloader, optimizer, scheduler, loss_f
         plt.clf()
         
    
-def test_model(args, model, dataloader, spm_model, writer, device):
+def test_model(args, model, dataloader, writer, device):
 
     model = model.eval()
     total_source_distance = 0
@@ -99,30 +125,44 @@ def test_model(args, model, dataloader, spm_model, writer, device):
     generated_series = []
 
     for batch_idx, batch in enumerate(tqdm(dataloader, desc='TEST Sequence')):
-        src_input = batch['src_input'].to(device)
-        tgt_input = batch['tgt_input'].to(device)
-        tgt_output = batch['tgt_output'].to(device)
-        timestamp = batch['time'].to(device)
+        src_input_1 = batch['src_input_1'].to(device)
+        tgt_input_1 = batch['tgt_input_!'].to(device)
+        tgt_output_1 = batch['tgt_output_1'].to(device)
+        src_input_2 = batch['src_input_2'].to(device)
+        tgt_input_2 = batch['tgt_input_2'].to(device)
+        tgt_output_2 = batch['tgt_output_2'].to(device)
+        timestamp = batch['timestamp'].to(device)
+        length = batch['length'].to(device)
+        timestamp = batch['timestamp'].to(device)
         length = batch['length'].to(device)
         reference = batch['target']
         source_distance = 0
 
         if args.vae_setting == True:
             with torch.no_grad():
-                src_embedding = model.get_embedding(src_input)
+                src_embedding = model.get_embedding(src_input_1)
 
-                src_mask = model.encoder.generate_square_subsequent_mask(src_input.size(1))
-                src_pad_mask = model.encoder.generate_padding_mask(src_input, 0)
+                src_mask = model.encoder.generate_square_subsequent_mask(src_input_1.size(1))
+                src_pad_mask = model.encoder.generate_padding_mask(src_input_1, 0)
 
                 z, mean, log_var = model.encoder(src_embedding, src_mask, src_pad_mask, timestamp)
-                output = model.decoder.vae_decode(z, src_pad_mask, timestamp)
+                output, generated = model.decoder.vae_decode(z, src_pad_mask, timestamp)
+            if batch_idx < 1:
+                print("timestamp")
+                print(timestamp)
+                print('target_input')
+                print(src_input_1)
+                print("output")
+                print(output)
+                print("generated")
+                print(generated)
 
         if args.vae_setting ==False:
             with torch.no_grad():
-                src_embedding = model.get_embedding(src_input)
-                src_mask = model.encoder.generate_square_subsequent_mask(src_input.size(1))
+                src_embedding = model.get_embedding(src_input_1)
+                src_mask = model.encoder.generate_square_subsequent_mask(src_input_1.size(1))
                 
-        for i in range(0, src_input.size(0)):
+        for i in range(0, src_input_1.size(0)):
             reference_series.append(reference[i])
             generated_series.append(output[i])
             source_distance = generated_series - reference_series
